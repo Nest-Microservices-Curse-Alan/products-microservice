f27d28ab970a9e2922846f828e955a64f478a3f8
Alan934 <sanjurjoalan77@gmail.com>
2024-09-17 23:10:50 -0300

    first commit

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,15 @@
+import { IsNumber, Min, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateProductDto {
+    
+    @IsString()
+    public name: string;
+
+    @IsNumber({
+        maxDecimalPlaces: 4,
+    })
+    @Min(0)
+    @Type(() => Number)
+    public price: number;
+}
